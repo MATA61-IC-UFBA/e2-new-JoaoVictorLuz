a1e29d5fe662af0a67605bb2750f087ef789075a
JoaoVictorLuz <joaovictorluzoliveira@gmail.com>
2024-08-12 16:41:20 -0300

    commit 1

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,27 @@
 /* main.c */
+#include <stdio.h>
+#include "token.h"
 
+extern int yylex();
+extern char *yytext;
+
+int main() {
+
+// nome da função que retorna o próximo token é yylex()
+//laço para chamar yylex() até o final da entrada
+
+    token_t token = -1;
+    while (token != EOL){
+        token = yylex();                        // call yylex
+        if (token == ERROR)
+            printf("erro léxico: %s\n", yytext);
+
+        else
+            if (token == NUMBER)
+            printf("constante decimal: %s\n", yytext);
+            else
+                printf("Código do token: %d\n", token);
+    }
+    
+
+}
